7a181c99df5727fd876113472e56b7c7306355ee
Mostafa Mohsin <mostafamohsin783@gmail.com>
2024-01-10 22:52:45 +0600

    datSet

--- index.js
+++ index.js
@@ -1,87 +1,97 @@
 const express = require('express');
 const cors = require('cors');
-const { query } = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('dotenv');
+const { json, urlencoded } = require('express');
+
+config(); // Load environment variables from .env file
+
 const app = express();
-require("dotenv").config();
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
-app.use(express.json())
-
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hkshaw4.mongodb.net/?retryWrites=true&w=majority`;
-
-// console.log(uri)
 const client = new MongoClient(uri, {
   serverApi: {
-    version: ServerApiVersion.v1,
+    version: '1',
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
-async function run() {
+async function connectToMongoDB() {
   try {
     await client.connect();
-   
-    const breakfast = client.db('Restaruant').collection('Breakfast');
-    const luanch = client.db('Restaruant').collection('Luanch');
-    const dinner = client.db('Restaruant').collection('Dinner');
-    const OrderList = client.db('Restaruant').collection('OrderList');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
 
-    app.get("/getBrakFast", async (req, res) => {
-      const data = breakfast.find();
-      const result = await data.toArray();
-      res.send(result);
-    });
-    app.get("/getLuanch", async (req, res) => {
-      const data = luanch.find();
-      const result = await data.toArray();
-      res.send(result);
-    });
-    app.get("/getDinner", async (req, res) => {
-      const data = dinner.find();
-      const result = await data.toArray();
-      res.send(result);
-    });
+async function closeMongoDBConnection() {
+  try {
+    await client.close();
+    console.log('Closed MongoDB connection');
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+  }
+}
 
-// end get main data from database
-// start get data from compunent for post
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.get('/getBrakFastInsert/:id', async(req,res) => {
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const result = await breakfast.findOne(query);
+app.get('/getBrakFast', async (req, res) => {
+  const result = await client.db('Restaruant').collection('Breakfast').find().toArray();
   res.send(result);
-})
+});
 
-app.get('/getLunchInsert/:id', async(req,res) => {
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const result = await luanch.findOne(query);
+app.get('/getLuanch', async (req, res) => {
+  const result = await client.db('Restaruant').collection('Luanch').find().toArray();
   res.send(result);
-})
-app.get('/getDinnerInsert/:id', async(req,res) => {
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const result = await dinner.findOne(query);
+});
+
+app.get('/getDinner', async (req, res) => {
+  const result = await client.db('Restaruant').collection('Dinner').find().toArray();
   res.send(result);
-})
+});
 
+app.get('/getBrakFastInsert/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await client.db('Restaruant').collection('Breakfast').findOne(query);
+  res.send(result);
+});
 
-app.post('/postData', async(req,res) => {
+app.get('/getLunchInsert/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await client.db('Restaruant').collection('Luanch').findOne(query);
+  res.send(result);
+});
+
+app.get('/getDinnerInsert/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await client.db('Restaruant').collection('Dinner').findOne(query);
+  res.send(result);
+});
+
+app.post('/postData', async (req, res) => {
   const body = req.body;
-  const query = await OrderList.insertOne(body);
+  const query = await client.db('Restaruant').collection('OrderList').insertOne(body);
   res.send(query);
-})
+});
 
-app.get("/getPostData", async (req, res) => {
+app.get('/getPostData', async (req, res) => {
   try {
     const { email } = req.query;
-    const booking = await OrderList.find({ email }).toArray();
+    const booking = await client.db('Restaruant').collection('OrderList').find({ email }).toArray();
     res.send(booking);
   } catch (error) {
     console.error(error);
@@ -89,34 +99,25 @@ app.get("/getPostData", async (req, res) => {
   }
 });
 
-app.delete("/deleteData/:id", async (req, res) => {
+app.delete('/deleteData/:id', async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
-  const result = await OrderList.deleteOne(query);
+  const result = await client.db('Restaruant').collection('OrderList').deleteOne(query);
   res.send(result);
 });
 
-
-
-
-  } 
-  finally {
-
-    // await client.close();
-  }
+// Start the server
+async function startServer() {
+  await connectToMongoDB();
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 }
-run().catch(console.dir);
 
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Gracefully handle termination signals and close MongoDB connection
+process.on('SIGINT', async () => {
+  await closeMongoDBConnection();
+  process.exit();
 });
 
-app.listen(port, () => {
-  console.log('Example app listening on port', port)
-});
+startServer();
